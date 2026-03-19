ef1c50a28e54003b674e64ed8c4f34536a2c1038
hpzerr91 <hans.mendo@efrei.net>
2026-03-19 12:01:34 +0100

    Ajout de la map(Tiled)
    changement du sprite 32x32 pour la demo

--- entities/player_top.py
+++ entities/player_top.py
@@ -1,5 +1,6 @@
 import pygame
-import sys
+import pytmx
+import pyscroll
 from entities.constante import Resolution
 
 # limite de l'ecran
@@ -8,8 +9,36 @@ SCREEN_WIDTH, SCREEN_HEIGHT = Resolution
 
 class Game:
     def __init__(self):
-        self.player = Player()
+        self.fenetre = pygame.display.set_mode(Resolution)
+        pygame.display.set_caption("Niveau Player Top")
 
+        tmx_data = pytmx.util_pygame.load_pygame("../assets/mapdessus.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.fenetre.get_size())
+
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.player = Player()
+        self.group.add(self.player)
+        self.clock = pygame.time.Clock()
+    def run(self):
+        running = True
+        while running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
+
+            self.group.update()
+            self.group.center(self.player.rect.center)
+
+            self.group.draw(self.fenetre)
+            pygame.display.flip()
+
+            self.clock.tick(60)
+
+        pygame.quit()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -18,21 +47,21 @@ class Player(pygame.sprite.Sprite):
         self.max_health = 100
         self.attack = 10
         self.speed = 2
+        self.sprite_sheet = pygame.image.load('../assets/RagerIdle.png')
 
-        sheet = pygame.image.load('../assets/ice_golem_v3_spritesheet.png').convert_alpha()
-
-        frame_width = 80
-        frame_height = 80
-        scale = 0.75
+        frame_width = 32
+        frame_height = 32
+        scale = 2
         frame_count = 5
 
         self.frames = []
         for i in range(frame_count):
-            frame = sheet.subsurface((i * frame_width, 0, frame_width, frame_height))
+            frame = self.sprite_sheet.subsurface((i * frame_width, 0, frame_width, frame_height))
             frame = pygame.transform.scale(
                 frame,
                 (int(frame_width * scale), int(frame_height * scale))
             )
+            frame.set_colorkey((0, 0, 0))
             self.frames.append(frame)
 
         self.current_frame = 0
@@ -43,6 +72,12 @@ class Player(pygame.sprite.Sprite):
 
         self.animation_speed = 0.14
         self.timer = 0
+
+    def get_image(self, x, y):
+        image= pygame.Surface([80,80])
+        image.blit(self.sprite_sheet, (0, 0), (x, y,80,80))
+        return image
+
 
     def move(self):
         keys = pygame.key.get_pressed()
@@ -72,34 +107,8 @@ class Player(pygame.sprite.Sprite):
 
 # Initialisation
 pygame.init()
-fenetre = pygame.display.set_mode(Resolution)
-pygame.display.set_caption("Niveau Player Top")
+
 horloge = pygame.time.Clock()
+game=Game()
+game.run()
 
-fond = pygame.image.load("../assets/Scene_Overview.png").convert()
-
-game = Game()
-all_sprites = pygame.sprite.Group(game.player)
-
-# Boucle principale
-running = True
-while running:
-
-    # EVENTS
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
-
-    # UPDATE
-    all_sprites.update()
-
-    # DRAW
-    fenetre.blit(fond, (-100, -50))
-    all_sprites.draw(fenetre)
-
-    pygame.display.flip()
-    horloge.tick(60)
-
-pygame.quit()
