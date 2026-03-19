0b2debed07b9cb9ddb29618a725bce729c375f5b
Mathieu SINGPRASEUTH <mathieu.singpraseuth@efrei.net>
2026-03-19 15:22:07 +0100

    Fix player jump height and adjust platform rectangle dimensions

--- entities/player_side.py
+++ entities/player_side.py
@@ -13,7 +13,7 @@ def lancer_jeu_side(fenetre):
     vitesse_x = 0 
     vitesse_y = 0 
     sol_rect = pygame.Rect(0, 550, Resolution[0], 20)
-    plateforme_rect = pygame.Rect(200, 350, 200, 20)
+    plateforme_rect = pygame.Rect(200, 350, 200, 40)
     
     nb_sauts = 0
     
@@ -60,9 +60,18 @@ def lancer_jeu_side(fenetre):
             golem_rect.bottom = plateforme_rect.top 
             nb_sauts = 0
 
-        if golem_rect.top < 0: #détection de collision pour le saut
+        if golem_rect.top < 0: #détection de collision pour le saut pour pas que le golem puisse sortir par le haut
             golem_rect.top = 0
+            vitesse_y = 1 #repart vers le bas immédiatement 
+
+        if golem_rect.top <= plateforme_rect.bottom and \
+        golem_rect.right >= plateforme_rect.left and \
+        golem_rect.left <= plateforme_rect.right and \
+        vitesse_y < 0 and golem_rect.top - vitesse_y >= plateforme_rect.bottom:
+            golem_rect.top = plateforme_rect.bottom
             vitesse_y = 1
+        
+        
 
         # DESSIN
         fenetre.fill((30, 30, 40))
@@ -115,5 +124,4 @@ def lancer_jeu_side(fenetre):
 
 
 
-
-
+    
