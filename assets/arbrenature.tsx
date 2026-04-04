4958c024d30f1905b8279f3111343016efe67756
Romain9294 <romain.pinard@efrei.net>
2026-04-04 17:42:25 +0200

    push de tout les assets en partie utilisé pour construire la map du niveau 2

---
+++ assets/arbrenature.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="arbrenature" tilewidth="32" tileheight="32" tilecount="432" columns="16">
+ <image source="deconature.png" width="512" height="864"/>
+</tileset>
