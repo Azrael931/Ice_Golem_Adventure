1199326f6423c8b50e8b245ee750363ebfd818da
hpzerr91 <hans.mendo@efrei.net>
2026-04-07 10:59:53 +0200

    avant nouvelle map tmx

--- assets/arbre.tsx
+++ assets/arbre.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="tons_of_tileset_5_10___cool_gaia_tiles_by_phyromatical_d81nzad" tilewidth="32" tileheight="32" tilecount="49" columns="7">
- <image source="../../../Downloads/tons_of_tileset_5_10___cool_gaia_tiles_by_phyromatical_d81nzad.png" width="240" height="240"/>
+<tileset version="1.10" tiledversion="1.12.0" name="tons_of_tileset_5_10___cool_gaia_tiles_by_phyromatical_d81nzad" tilewidth="32" tileheight="32" tilecount="2200" columns="44">
+ <image source="tons_of_tileset_5_10___cool_gaia_tiles_by_phyromatical_d81nzad.png" width="1408" height="1600"/>
 </tileset>
--- assets/assetesolnature.tsx
+++ assets/assetesolnature.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="assetesolnature" tilewidth="32" tileheight="32" tilecount="180" columns="20">
- <image source="../../../Downloads/RPG Nature Tileset.png" width="641" height="288"/>
+<tileset version="1.10" tiledversion="1.12.0" name="assetesolnature" tilewidth="32" tileheight="32" tilecount="1728" columns="57">
+ <image source="dot_day17___evolina_ice_cave_by_phyromatical_das20xd.png" width="1824" height="1664"/>
 </tileset>
